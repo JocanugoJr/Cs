b8252d376f3ccef0b3e4f9a10bbdf842a816d933
JocanugoJr <91928006+JocanugoJr@users.noreply.github.com>
2021-10-10 19:12:48 -0300

    modificação

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace doKurso
+{
+    class Program
+    {
+        static bool minimus(int a, int b)
+        {
+            if(a>24 || a<0)
+            {
+                return true;
+            }
+            if(b>24 || b<0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe os Valores de entrada:");
+            string[] vet;
+            int a, b;
+
+            do
+            {
+                vet = Console.ReadLine().Split(' ');
+                a = int.Parse(vet[0]);
+                b = int.Parse(vet[1]);
+
+            } while (minimus(a,b)==true);
+                
+            int horas = 24;
+            int minuto;
+
+
+
+            if (a < b)
+            {
+                horas = b - a;
+            }
+            else if(a>b)
+            {
+                horas = (horas - a) - b; 
+            }
+
+            minuto = horas * 60;
+            Console.WriteLine("O JOGO DUROU " + horas + " HORAS OU " + minuto + " MINUTOS.");
+        }
+    }
+}
