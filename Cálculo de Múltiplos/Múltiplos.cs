9791433514e7fb35650114f1658ca3e6a3e1663a
JocanugoJr <91928006+JocanugoJr@users.noreply.github.com>
2021-10-11 12:17:25 -0300

    Cálculo de Múltiplos C#

---
+++ Cálculo de Múltiplos/Múltiplos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace doKurso
+{
+    class Program
+    {
+        //public static decimal Abs(decimal value);
+        static bool Multiplus(int a, int b)
+        {
+            if (Math.Abs(a) > Math.Abs(b))
+            {
+                if (a % b == 0)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (b % a == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Informe os Valores de entrada:");
+            
+            int a, b;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            if (Multiplus(a, b) == true)
+            {
+                Console.WriteLine("São Múltiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são Múltiplos");
+            }
+
+        }
+
+        //
+    }
+}
