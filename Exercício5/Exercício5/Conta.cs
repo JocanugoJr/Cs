fcd45b42b07d41e4555359a99c62a66dd29c6373
JocanugoJr <91928006+JocanugoJr@users.noreply.github.com>
2021-10-22 20:31:44 -0300

    Comeco_OO2
    uma versão mais elaborada do programa de Cálculo de Contas.

---
+++ Exercício5/Exercício5/Conta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício5
+{
+    class Conta
+    {
+
+        //Private
+        private const double _taxa = 5.00;//taxa para o saque
+        
+        //Publics e Properties
+        public int Num {//o número da conta, o usuário não pode mudar, apenas vê-lo
+            get;
+            private set;//para dizer que o número nunca poderá ser mudado.
+        }
+        public string Nome//se o usuário quiser editar, ele consegue
+        {
+            get; 
+            set;
+        }
+        public double Saldo 
+        { 
+            get; 
+            private set; 
+        }
+
+        //Construtores
+        public Conta(int num, string nome)
+        {
+            Num = num;
+            Nome = nome;
+        }
+        public Conta(int num, string nome, double depositoInic): this(num, nome)
+        {
+            Aumenta(depositoInic);//o ideal não é atribuir, o ideal é somar ao saldo.
+        }
+
+        //Métodos
+        public void Aumenta(double deposito)//o saldo só aumenta por depósito
+        {
+            Saldo += deposito;
+        }
+        public void Sacar(double saque)//pode ficar com saldo negativo.
+        {
+            Saldo -= (_taxa + saque);
+        }
+
+        public override string ToString()
+        {
+            return "Conta "+Num
+                +", Titular: "+Nome
+                +", Saldo: $ "+Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercício5/Exercício5/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício5
+{
+    class Program
+    {
+        static void imprime(Conta pessoa)
+        {
+            Console.WriteLine("Dados atualizados:");
+            Console.WriteLine(pessoa);
+            Console.WriteLine();
+        }
+        static void Main()
+        {
+            Conta pessoa;
+
+            Console.WriteLine("Entre com os dados da conta:");
+            Console.Write("Número da Conta: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome do Titular: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Deseja fazer um depósito inicial? (s/n)");
+            char resposta;
+
+            do
+            {
+                resposta = char.Parse(Console.ReadLine());//como se lê um char
+                if (resposta == 's')
+                {
+                    Console.Write("Insira o valor de depósito inicial: ");
+                    double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    pessoa = new Conta(num, nome, deposito);
+                }
+                else
+                {
+                    pessoa = new Conta(num, nome);
+                }
+            } while (resposta != 's' && resposta != 'n');
+
+            imprime(pessoa);
+
+            while (true)
+            {
+                Console.WriteLine("Deseja:");
+                Console.WriteLine("1-Depositar;");
+                Console.WriteLine("2-Sacar;");
+                Console.WriteLine("3-Sair");
+                int option = int.Parse(Console.ReadLine());
+                if (option == 1)
+                {
+                    Console.Write("Entre com um valor para depósito: ");
+                    double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    pessoa.Aumenta(deposito);
+                }
+                else if(option == 2)
+                {
+                    Console.Write("Entre com um valor para saque: ");
+                    double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    pessoa.Sacar(saque);
+                }
+                else
+                {
+                    break;
+                }
+
+                imprime(pessoa);
+            }
+
+            imprime(pessoa);
+        }
+    }
+}
